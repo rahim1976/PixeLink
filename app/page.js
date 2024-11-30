fce85e99de1ddfc2f9f84ace46b4f3df0a2c90ce
Rahim Ali <139705342+rahim1976@users.noreply.github.com>
2024-12-01 02:27:46 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -37,7 +37,7 @@ export default function Home () {
       <section className='container px-4 py-10 mx-auto lg:h-128 lg:space-x-8 lg:flex lg:items-center'>
         <div className='w-full text-center lg:text-left lg:w-1/2 lg:-mt-8'>
           <h1 className='text-3xl leading-snug text-gray-800 dark:text-gray-200 md:text-4xl'>
-            Create Your Online Presense With with <br className='hidden lg:block' />{' '}
+            Create Your Online Presense With <br className='hidden lg:block' />{' '}
             <span className='font-semibold'>PixeLink</span> in Web Development,{' '}
             <br className='hidden lg:block' />Including{' '}
             <span className='font-semibold underline decoration-primary'>
